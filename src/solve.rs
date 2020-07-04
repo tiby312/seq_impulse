7bf194188f432ad38b2a49e87cbbcd10ee3c2071
Ken <kenakioreed@gmail.com>
2020-07-04 18:20:23 -0400

    added serialization

--- src/solve.rs
+++ src/solve.rs
@@ -1,3 +1,5 @@
+use serde::{Serialize, Deserialize};
+
 use axgeom::*;
 use axgeom::Axis;
 use std::collections::BTreeMap;
@@ -6,10 +8,11 @@ use duckduckgeo::grid;
 
 use bothash::BotCollisionHash;
     mod bothash{
+        use serde::{Serialize, Deserialize};
         use core::cmp::Ordering;
         use core::marker::PhantomData;
-        #[derive(Copy,Clone)]
-        pub struct BotCollisionHash<T>((usize,usize),PhantomData<T>);
+        #[derive(Hash,Serialize,Deserialize,Clone,Debug)]
+        pub struct BotCollisionHash<T>(u32,PhantomData<T>);
         
          impl<T> PartialEq for BotCollisionHash<T> {
             fn eq(&self, other: &Self) -> bool {
@@ -28,15 +31,20 @@ use bothash::BotCollisionHash;
             }
         }
         impl<T> BotCollisionHash<T>{
-            pub fn new(a:&T,b:&T)->BotCollisionHash<T>{                
-                let a=a as *const _ as usize;
-                let b=b as *const _ as usize;
-                let [a,b]=if a<b{
-                    [a,b]
-                }else{
+            pub fn new(base:usize,a:&T,b:&T)->BotCollisionHash<T>{                
+                
+                let a=((a as *const _ as usize - base )/core::mem::size_of::<T>()) as u32;
+                let b=((b as *const _ as usize - base )/core::mem::size_of::<T>()) as u32;
+                
+                
+                let [a,b]=if a>b{ //TODO important that else defaults to not moving?
                     [b,a]
+                }else{
+                    [a,b]
                 };
-                BotCollisionHash((a,b),PhantomData)
+                
+            
+                BotCollisionHash((a<<16) | b,PhantomData)
             }
         }
     }
@@ -59,25 +67,54 @@ impl BotCollisionHash{
 }
 */
 
-#[derive(PartialOrd,PartialEq,Eq,Ord,Copy,Clone)]
-struct WallCollisionHash{
-    a:usize,
+use core::marker::PhantomData;
+
+#[derive(Hash,Serialize,Deserialize,Debug,PartialOrd,PartialEq,Eq,Ord,Copy,Clone)]
+struct WallCollisionHashInner{
+    a:u16,
     dir:grid::CardDir
 }
 
-fn single_hash<T>(a:&T,dir:grid::CardDir)->WallCollisionHash{
-    WallCollisionHash{a:a as *const _ as usize,dir}
+#[derive(Hash,Serialize,Deserialize,Debug,Copy,Clone)]
+struct WallCollisionHash<T>(
+    WallCollisionHashInner,
+    PhantomData<T>
+);
+use core::cmp::Ordering;
+
+impl<T> PartialEq for WallCollisionHash<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+impl<T> Eq for WallCollisionHash<T> {}
+
+impl<T> PartialOrd for WallCollisionHash<T>{
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering>{
+        self.0.partial_cmp(&other.0)
+    }
+}
+impl<T> Ord for WallCollisionHash<T>{
+    fn cmp(&self, other: &Self) -> Ordering{
+        self.0.cmp(&other.0)
+    }
 }
 
 
-
-pub struct CollisionVelocitySolver<T>{
-    last_bot_col:BTreeMap<BotCollisionHash<T>,f32>,
-    last_wall_col:BTreeMap<WallCollisionHash,f32>
+fn single_hash<T>(base:usize,a:&T,dir:grid::CardDir)->WallCollisionHash<T>{
+    let a=((a as *const _ as usize - base) / core::mem::size_of::<T>()) as u16; 
+    WallCollisionHash(WallCollisionHashInner{a,dir},PhantomData)
 }
 
-impl<T:Send+Sync> CollisionVelocitySolver<T>{
-    pub fn new()->CollisionVelocitySolver<T>{
+
+#[derive(Clone,Hash,Serialize,Deserialize,Debug)]
+pub struct CollisionVelocitySolver<T,K>{
+    last_bot_col:BTreeMap<BotCollisionHash<T>,K>,
+    last_wall_col:BTreeMap<WallCollisionHash<T>,K>
+}
+
+impl<T:Send+Sync+core::fmt::Debug> CollisionVelocitySolver<T,f32>{
+    pub fn new()->CollisionVelocitySolver<T,f32>{
         CollisionVelocitySolver{last_bot_col:BTreeMap::new(),last_wall_col:BTreeMap::new()}
     }
 
@@ -97,18 +134,20 @@ impl<T:Send+Sync> CollisionVelocitySolver<T>{
         let bias_factor=0.3;
         let allowed_penetration=radius*0.2;
         let num_iterations=5;
-    
+        let base=tree.get_bots().as_ptr() as usize;
+
         let mut collision_list={
             let ka3 = &self.last_bot_col;
-            tree.collect_intersections_list_par(|a,b|{
+            tree.collect_intersections_list_par(|a:&mut T,b:&mut T|{
                 let p2=*pos_func(b);
                 let p1=*pos_func(a);
                 let offset=p2-p1;
                 let distance2=offset.magnitude2();
+                
                 if distance2<diameter2{
                     let distance=distance2.sqrt();
                     
-                    let offset_normal=if distance<0.00001{
+                    let offset_normal=if distance<0.0001{
                         //Make a random direction
                         let rot=p1.y*2.0-p1.x;
                         vec2(rot.cos(),rot.sin())
@@ -119,11 +158,12 @@ impl<T:Send+Sync> CollisionVelocitySolver<T>{
                     let separation=(diameter-distance)/2.0;
                     let bias=-bias_factor*(1.0/num_iterations as f32)*( (-separation+allowed_penetration).min(0.0));
                     
-                    let hash=BotCollisionHash::new(a,b);
+                    let hash=BotCollisionHash::new(base,a,b);
+                    //let impulse=20.0;
+                    //let impulse=if true{
                     let impulse=if let Some(&impulse)=ka3.get(&hash){ //TODO inefficient to check if its none every time
+                    
                         let k=offset_normal*impulse;
-                        let va=vel_func(a);
-                        let vb=vel_func(b);
                         
                         *vel_func(a)-=k;
                         *vel_func(b)+=k;
@@ -146,16 +186,17 @@ impl<T:Send+Sync> CollisionVelocitySolver<T>{
             collisions:[Option<(f32,Vec2<f32>,grid::CardDir,f32)>;2],
         }
 
+        
         let mut wall_collisions={
             let ka3 = &self.last_wall_col;
-
+            
             tree.collect_all_par(|rect,a|{
                 
                 let arr=duckduckgeo::grid::collide::is_colliding(&walls,&grid_viewport,rect.as_ref(),radius);
                 let create_collision=|bot:&mut T,dir:grid::CardDir,seperation:f32,offset_normal:Vec2<f32>|{
                     let bias=-bias_factor*(1.0/num_iterations as f32)*( (-seperation+allowed_penetration).min(0.0));
 
-                    let impulse=if let Some(&impulse)=ka3.get(&single_hash(bot,dir)){ //TODO inefficient to check if its none every time
+                    let impulse=if let Some(&impulse)=ka3.get(&single_hash(base,bot,dir)){ //TODO inefficient to check if its none every time
                         let k=offset_normal*impulse;
                         *vel_func(bot)+=k;
                         impulse
@@ -187,10 +228,14 @@ impl<T:Send+Sync> CollisionVelocitySolver<T>{
                 }
             })
         };
+        
 
+        
         for _ in 0..num_iterations{
 
-            collision_list.for_every_pair_mut_par(tree,|a,b,&mut (offset_normal,bias,ref mut acc)|{
+            
+            
+            collision_list.for_every_pair_mut(tree,|a,b,&mut (offset_normal,bias,ref mut acc)|{
                 
                 let vel=*vel_func(b)-*vel_func(a);
 
@@ -202,11 +247,14 @@ impl<T:Send+Sync> CollisionVelocitySolver<T>{
                 let impulse=*acc-p0;
                 
                 let k=offset_normal*impulse;
+                //assert!(!k.is_nan());
+                //println!("{:?}",k);
                 *vel_func(a)-=k;
                 *vel_func(b)+=k;
-            });     
-
-            wall_collisions.for_every_mut_par(tree,|bot,wall|{
+            });
+                 
+            
+            wall_collisions.for_every_mut(tree,|bot,wall|{
                 
                 
                 for k in wall.collisions.iter_mut(){
@@ -222,24 +270,41 @@ impl<T:Send+Sync> CollisionVelocitySolver<T>{
                     }
                 }; 
             })
-        }  
+            
+            
+            
+            
+        } 
+         
 
 
         self.last_bot_col.clear();
         self.last_wall_col.clear();
 
-        let (ka2,ka3):(BTreeMap<_,_>,BTreeMap<_,_>)=rayon::join(||{
+        let (ka2,ka3):(BTreeMap<_,_>,_)=rayon::join(||{
+            
             collision_list.get(&tree).iter().flat_map(|a|a.iter()).map(|(a,b,(_,_,impulse))|{
+                (BotCollisionHash::new(base,*a,*b),*impulse)
+            }).collect()
+            
+            
+            /*
+            collision_list.get(&tree).iter().map(|(a,b,(_,_,impulse))|{
                 (BotCollisionHash::new(*a,*b),*impulse)
             }).collect()
+            */
+            
+            
         },
         ||{
+            
             wall_collisions.get(&tree).iter().flat_map(|(bot,wall)|{
                 let k=wall.collisions.iter().filter(|a|a.is_some()).map(|a|a.unwrap());
                 k.map(move |(_,_,dir,impulse)|{
-                    (single_hash(bot,dir),impulse)
+                    (single_hash(base,*bot,dir),impulse)
                 })
             }).collect()
+            
         });
 
         self.last_bot_col=ka2;
